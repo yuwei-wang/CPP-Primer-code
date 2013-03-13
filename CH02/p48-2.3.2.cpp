fbe4596b161e03e9f71b1ebb557a8f3c887234d1
Yu-Wei Wang <user@fake.ywwang.idv.tw>
2013-03-13 18:12:58 +0800

    20130313-1812
    CH02

---
+++ CH02/p48-2.3.2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	//int double = 3.14159; //double is a keyword
+	char _;
+	//bool catch-22; //catch is a keyword
+	//char 1_or_2 = '1';  //can't start with number
+	float Float =3.14f;
+	
+	return 0;
+}
