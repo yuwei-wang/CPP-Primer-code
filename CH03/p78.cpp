6494422afb2f159720a2f684d4730ddeb733659d
Yu-Wei Wang <user@fake.ywwang.idv.tw>
2013-03-20 06:44:20 +0800

    20130320-0644
    CH03

---
+++ CH03/p78.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+using std::cin;
+using std::string;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	string S;
+	cin >> S;
+	cout << S << endl;
+	
+	return 0;
+}
